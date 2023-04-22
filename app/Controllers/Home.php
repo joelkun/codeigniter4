7d8079c06067b2a45c83be251a90c5e4368dbf76
joelkun <47799679+joelkun@users.noreply.github.com>
2023-04-22 17:44:42 -0500

    pasar datos dinamicos

--- app/Controllers/Home.php
+++ app/Controllers/Home.php
@@ -6,7 +6,11 @@ class Home extends BaseController
 {
     public function index()
     {
-        return view('welcome_message');
+        $dato =[
+                   "titulo"=> "Inicio de codeigniter 4"
+                 ];
+
+        return view('welcome_message', $dato);
     }
     public function contacto(){
         return view('contactoPrueba');
